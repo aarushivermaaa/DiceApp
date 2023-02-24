283f969801723eecc68cd2dfb34d2307db2dbe07
aarushivermaaa <aarushi.verma2021@vitbhopal.ac.in>
2023-02-24 20:54:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DiceApp
+ This is a GitHub repository for a dice app made using Flutter on Android Studio. The app features two dice that can be rolled by tapping on them, with randomized values. To get started with the app, users need to have Flutter and Android Studio installed on their system. Contributions are welcome!
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.purple,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage>{
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace(){
+    setState(() {
+      leftDiceNumber = Random(rightDiceNumber =Random().nextInt(6) + 1).nextInt(6)+1;
+    });
+
+  }
+@override
+Widget build(BuildContext context) {
+  return Center(
+    child: Row(
+      children: <Widget>[
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              changeDiceFace();
+              print('DiceNumber= $leftDiceNumber');
+            },
+            child: Image.asset('images/dice$leftDiceNumber.png'),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              changeDiceFace();
+              print('DiceNumber = $rightDiceNumber');
+            },
+            child: Image.asset('images/dice$rightDiceNumber.png'),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+}
